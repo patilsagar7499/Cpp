ce0106a8c9335536e29121a71e93427a39aab4ca
Sagar Patil <sagar.22111052@viit.ac.in>
2025-01-29 10:14:04 +0530

    Update Tut49.cpp

--- C++/Tut49.cpp
+++ C++/Tut49.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 using namespace std;
 class Test{
-    public:
     int a,b;
+    public:
+    
     Test(int i,int j):a(i),b(j){
         cout<<"a:"<<a<<endl;
         cout<<"b:"<<b<<endl;
     }
+    // Test(int i,int j):a(i),b(j+2) -->Works fine
+    // Test(int i,int j):a(i),b(j+a) -->Works fine
+    // Test(int i,int j):b(i),a(j) -->Works fine
+    // Test(int i,int j):b(i),b(j+2) -->Works fine
+    // Test(int i,int j):b(i),b(j+a) -->Doesn't work fine as a is initialized before b!
 };
 int main()
 {
     Test t(1,2);
 return 0;
 }
+
+
